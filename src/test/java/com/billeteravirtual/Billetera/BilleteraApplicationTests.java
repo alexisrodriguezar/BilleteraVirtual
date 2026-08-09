db1435f4e4540c311e8e67ad4bdd0aca34b6b238
alexisrodriguezar <alexisrodriguez200299@gmail.com>
2026-08-09 01:05:25 -0300

    build: limpieza de arquitectura legacy e inicialización de proyecto Spring Boot

---
+++ src/test/java/com/billeteravirtual/Billetera/BilleteraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billeteravirtual.Billetera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleteraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
