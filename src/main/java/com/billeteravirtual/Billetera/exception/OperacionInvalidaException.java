55f74b099884673df7a29555d640ab5c3c4e9d6b
alexisrodriguezar <alexisrodriguez200299@gmail.com>
2026-08-10 00:38:16 -0300

    feat: agregar excepciones de dominio

---
+++ src/main/java/com/billeteravirtual/Billetera/exception/OperacionInvalidaException.java
@@ -0,0 +1,8 @@
+package com.billeteravirtual.Billetera.exception;
+
+public class OperacionInvalidaException extends RuntimeException {
+    public OperacionInvalidaException(String mensaje) {
+
+        super(mensaje);
+    }
+}
---
+++ src/main/java/com/billeteravirtual/Billetera/exception/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package com.billeteravirtual.Billetera.exception;
+
+public class SaldoInsuficienteException extends RuntimeException {
+    public SaldoInsuficienteException(String mensaje) {
+
+        super(mensaje);
+    }
+}
