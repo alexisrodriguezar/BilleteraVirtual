b61e354fb37f7e78e36713805784d093955102de
alexisrodriguezar <alexisrodriguez200299@gmail.com>
2026-08-10 00:39:51 -0300

    feat: agregar findByDni a IUsuarioRepository

--- src/main/java/com/billeteravirtual/Billetera/repository/IUsuarioRepository.java
+++ src/main/java/com/billeteravirtual/Billetera/repository/IUsuarioRepository.java
@@ -2,6 +2,9 @@ package com.billeteravirtual.Billetera.repository;
 
 import com.billeteravirtual.Billetera.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByDni(String dni);
 }
